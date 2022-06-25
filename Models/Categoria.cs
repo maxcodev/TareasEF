ddaeacd4dda5b97065988cb474acae7fd263176e
Máximo Sánchez <id569723@outlook.com>
2022-06-25 11:05:38 -0500

    TEST: Model was overwritten by FLUENT API in Context

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,11 +4,13 @@ namespace EntityFramework.Models;
 
 public class Categoria
 {
-    [Key]
+    //[Key]
     public Guid CategoriaId { get; set; }
-    [Required]
-    [MaxLength(150)]
+    
+    //[Required]
+    //[MaxLength(150)]
     public string Nombre { get; set; }
+    
     public string Descripcion { get; set; }
     
     public virtual ICollection<Tarea> Tareas { get; set; }
--- TareasContext.cs
+++ TareasContext.cs
@@ -9,4 +9,27 @@ public class TareasContext : DbContext
     public DbSet<Tarea> Tareas { get; set; }
     
     public TareasContext(DbContextOptions<TareasContext> options) :base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(categoria =>
+        {
+            categoria.ToTable("Categoria");
+            categoria.HasKey(c => c.CategoriaId);
+            categoria.Property(c => c.Nombre).IsRequired().HasMaxLength(150);
+            categoria.Property(c => c.Descripcion);
+        });
+
+        modelBuilder.Entity<Tarea>(tarea =>
+        {
+            tarea.ToTable("Tarea");
+            tarea.HasKey(t => t.TareaId);
+            tarea.HasOne(t => t.Categoria).WithMany(t => t.Tareas).HasForeignKey(t => t.CategoriaId);
+            tarea.Property(t => t.Titulo).IsRequired().HasMaxLength(200);
+            tarea.Property(t => t.Descripcion);
+            tarea.Property(t => t.PrioridadTarea);
+            tarea.Property(t => t.FechaCreacion);
+            tarea.Ignore(t => t.Resumen);
+        });
+    }
 }
